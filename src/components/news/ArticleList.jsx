27761bfd02887cd135b31121f71f1c7f4252df28
Sofia Tejada <sofianais@icloud.com>
2021-09-15 17:15:54 -0700

    more stuff written

---
+++ src/components/news/ArticleList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import Article from './Article';
+import { Link } from 'react-router-dom';
+
+const ArticleList = ({ articles }) => (
+  <ul aria-label="articles">
+    {articles.map}
+    <li>
+
+    </li>
+  </ul>
+);
